1ea7a50b1cce8dd121b7e174f8020a8d3dc2fa31
Mary Hsieh <a5925457@gmail.com>
2022-03-13 22:02:30 +0800

    update

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
+const cors = require('cors');
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello</h1>");
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello</h1>');
 });
 
 // put router here
-app.use("/api/restaurant", require("./routes/restaurant"));
-app.use("/api/restaurant-pic", require("./routes/restaurant-pic"));
-app.use("/api/spmenu", require("./routes/spmenu"));
-app.use("/api/menu-pic", require("./routes/menu-pic"));
+app.use('/api/restaurant', require('./routes/restaurant'));
+app.use('/api/restaurant-pic', require('./routes/restaurant-pic'));
+app.use('/api/spmenu', require('./routes/spmenu'));
+app.use('/api/menu-pic', require('./routes/menu-pic'));
 
 app.use((req, res) => {
-  res.status(404).send("<h1>404</h1>");
+    res.status(404).send('<h1>404</h1>');
 });
 
 app.listen(3000, () => {
-  console.log("Server is running");
+    console.log('Server is running');
 });
